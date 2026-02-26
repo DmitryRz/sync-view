f9289fd69af1f0f10932fe94f4daa57707984d49
Dmitry Rzhenev <rzhenev.116@gmail.com>
2026-02-27 00:13:33 +0300

    feat: инициализация spring boot (backend)

---
+++ server/sync-view-platform/src/main/java/io/github/dmitryrz/syncview/SyncViewApplication.java
@@ -0,0 +1,13 @@
+package io.github.dmitryrz.syncview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncViewApplication.class, args);
+	}
+
+}
---
+++ server/sync-view-platform/src/test/java/io/github/dmitryrz/syncview/SyncViewApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dmitryrz.syncview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
