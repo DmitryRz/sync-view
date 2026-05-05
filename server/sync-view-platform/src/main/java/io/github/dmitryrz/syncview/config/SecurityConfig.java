be2534ae0c0bafd504862b0cd77fab2b7f55c2a1
Dmitry Rzhenev <rzhenev.116@gmail.com>
2026-05-06 00:18:34 +0300

    feat(server/security): настроить правила доступа для эндпоинтов

--- server/sync-view-platform/src/main/java/io/github/dmitryrz/syncview/config/SecurityConfig.java
+++ server/sync-view-platform/src/main/java/io/github/dmitryrz/syncview/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.github.dmitryrz.syncview.config;
 import io.github.dmitryrz.syncview.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,6 +22,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/rooms/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/rooms/**").authenticated()
+                        .requestMatchers(HttpMethod.PUT, "/rooms/**").authenticated()
+                        .requestMatchers(HttpMethod.DELETE, "/rooms/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
